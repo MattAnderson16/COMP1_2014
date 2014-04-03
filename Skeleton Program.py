cf9dfac68a5491e9ee741f8a66ecf61319351b96
MattAnderson16 <30393@longroad.ac.uk>
2014-04-03 13:51:20 +0100

    Task 4
    Task 4 leaderboard display improvement

--- Skeleton Program.py
+++ Skeleton Program.py
@@ -173,12 +173,9 @@ def DisplayRecentScores(RecentScores):
   print()
   print('Recent Scores: ')
   print()
-  print("------------------")
   print("|{0:<10}|{1:<5}|".format("Name","Score"))
-  print("------------------")
   for Count in range(1, NO_OF_RECENT_SCORES + 1):
     print("|{0:<10}|{1:<5}|".format(RecentScores[Count].Name,RecentScores[Count].Score))
-    print("------------------")
   print()
   print('Press the Enter key to return to the main menu')
   input()
